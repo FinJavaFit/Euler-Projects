e95f0a52b7fdaff37f2359e51e4c4ffbec8bd611
FinJavaFit <chris0_23@yahoo.com>
2016-05-26 21:12:13 -0500

    Update Q2.java
    This one wasn't as hard as the first. The biggest problem I had with it was getting the loop intervals correct, but the if statement logic was pretty straight forward.

--- Q2.java
+++ Q2.java
@@ -12,28 +12,28 @@ public class MOOO {
        
         int sum = 0;
         int sum1 = 0;
-        int sum2 = 0;
+       
       
         
-        for (int i = 1; i < 4000000; i+=sum ) {
+        for (int i = 1; i < 4000000; i+=sum ) {//scanner could replace "4,000,000" in for loop for user input
             sum += i;
          
             
-          if ((i % 2) == 0) {
+          if ((i % 2) == 0) { //checks if int is even
                 
                 sum1 = sum1 + i;
                
             }
             
-          else if ((sum % 2)== 0) {
+          else if ((sum % 2)== 0) {//checks if int is even
                 sum1 = sum1 + sum;    
             }
            
-             System.out.println(i + " " + sum);
+             System.out.println(i + " " + sum);//prints out each number (for the sake of making sure pattern is correct)
             }
              
             
-     System.out.println("Final sum1 = " + sum1);
+     System.out.println("Final sum1 = " + sum1);//prints sum1 after loop is completed
     }
     
 }
